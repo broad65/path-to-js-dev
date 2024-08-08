f5bda0b35fecd1784b9fd3b4404c5782ed9820c7
broad65 <odhiambobroad65@gmail.com>
2024-08-08 22:02:55 +0300

    differen data types

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 console.log('welcome to thirty days of javascript');
+
+/*string data type*/
+let myString ="javascript";
+console.log(myJavascript);  //javacript
+
+//string concatenation
+//Connecting two or more strings together is called concatenation. 
+let firstName="abela";
+let lastName="achieng";
+
+let fullName="firstName+space+lastName";
+console.log(fullName);
+
+//Accessing characters in a string: We can access each character in a string using its index. In programming, counting starts from 0. The first index of the string is zero, and the last index is the length of the string minus one.
+let string='javascript'
+firstLetter=[0]
+console.log(firstLetter)
+
+/*boolean*/
+
+let lightOff = " false";
+let lightOn = "true";
+console.log(lightOn); //true
+
+/*Array*/
+//An array is a list of data values in a square bracket.
+let nums=[0,1,3]
+nums=10
+console.log(nums);  //10,1,3
+
+
+
